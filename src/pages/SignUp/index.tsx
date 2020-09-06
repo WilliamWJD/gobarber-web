53166a35b7d2ab9ea3ca89738a7d3488d62c0be3
WilliamWJD <william.jose.wjd@gmail.com>
2020-09-06 11:33:49 -0300

    finalizando a estilização da página de login

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,6 +3,7 @@ import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -32,9 +33,7 @@ const SignUp: React.FC = () => {
       });
     } catch (err) {
       const errors = getValidationErrors(err);
-
       formRef.current?.setErrors(errors);
-
       console.log(err);
     }
   }, []);
